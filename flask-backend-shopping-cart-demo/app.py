2b8eff2f1f8bdc0fa4eaed6116f8c5c77f4021d8
sameergupta1499 <sameergupta1499@gmail.com>
2020-04-02 00:10:53 +0530

    Added default values for the args value or requestDict.

--- flask-backend-shopping-cart-demo/app.py
+++ flask-backend-shopping-cart-demo/app.py
@@ -3,11 +3,11 @@ from mongoDatabase.app import requestDB
 import json
 
 requestDict = {
-        "RequestType": "",                    # possible values are GetProductData and GetBrandsName
+        "RequestType": "GetProducts",                    # possible values are GetProductData and GetBrandsName
         "Filters":[] ,     #Possible Values PriceAscending,PriceDescending,BrandsFilter .    # All string type, it just indicates what filter have been applied.
         "BrandsFilter": [],                                          #will only be present in the request if BrandsFilterExists.
         "CurrentPage": 1,
-        "ProductPerPage": 5
+        "ProductPerPage": 25
     }
 print(requestDB(requestDict))
 
@@ -15,24 +15,26 @@ app = Flask(__name__)
 #app.run(debug=True)
 
 def initializeRequestDict(args):
-    requestDict["RequestType"] = args["RequestType"]   #args are the parameters after ?
-    requestDict["Filters"] = json.loads(args["Filters"])
-    requestDict["BrandsFilter"] = json.loads(args["BrandsFilter"])
-    requestDict["CurrentPage"] = int(args["CurrentPage"])
-    requestDict["ProductPerPage"] = int(args["ProductPerPage"])
+    #true_val if condition else false_val
+    requestDict["RequestType"] = args["RequestType"] if ("RequestType" in args) else "GetProducts" #args are the parameters after ?
+
+    requestDict["Filters"] = json.loads(args["Filters"]) if ("Filters" in args) else []
+    requestDict["BrandsFilter"] = json.loads(args["BrandsFilter"]) if ("BrandsFilter" in args) else []
+    requestDict["CurrentPage"] = int(args["CurrentPage"]) if ("CurrentPage" in args) else 1
+    requestDict["ProductPerPage"] = int(args["ProductPerPage"]) if ("ProductPerPage" in args) else 25
 
 
 @app.route('/', methods=('GET', 'POST'))
-def hello_world():
-    if request.method == 'GET':
-        print("hello")
-        print(request.args["RequestType"])
-        return jsonify({'sameer': 'sameer'})
-
-    if request.method == 'POST':
-        jsonData = request.get_json()
-        print(jsonData)
-        return jsonify({'sameer': 'sameer'})
+def home():
+    requestDict = {
+        "RequestType": "GetProducts",  # possible values are GetProductData and GetBrandsName
+        "Filters": [],
+        # Possible Values PriceAscending,PriceDescending,BrandsFilter .    # All string type, it just indicates what filter have been applied.
+        "BrandsFilter": [],  # will only be present in the request if BrandsFilterExists.
+        "CurrentPage": 1,
+        "ProductPerPage": 25
+    }
+    return jsonify(requestDB(requestDict))
 
 
 @app.route('/Page=<page>', methods=('GET', 'POST'))
--- flask-backend-shopping-cart-demo/temp.py
+++ flask-backend-shopping-cart-demo/temp.py
@@ -1,9 +1,21 @@
-import json
+requestDict = {
+        "RequestTypea": "GetProducts",                    # possible values are GetProductData and GetBrandsName
+        "Filters":["BrandsFilter"] ,     #Possible Values PriceAscending,PriceDescending,BrandsFilter .    # All string type, it just indicates what filter have been applied.
+        "BrandsFilter": ["Samsung","Mi"],                                          #will only be present in the request if BrandsFilterExists.
+        "CurrentPage": 1,
+        "ProductPerPage": 5
+}
+
+print("RequestType" in requestDict)
+
+
+
+"""import json
 
 str='["Samsung","Mi"]'
 str = json.loads(str)
 print(str)
-"""from mongoDatabase.app import requestDB
+from mongoDatabase.app import requestDB
 
 requestDict = {
         "RequestType": "GetProducts",                    # possible values are GetProductData and GetBrandsName
